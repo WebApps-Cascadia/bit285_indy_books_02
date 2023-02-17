cd7a5c7f762f4463968f47d5e21eb05e2dca1f8d
Shelby Holmes <shelbynholmes@gmail.com>
2023-02-16 18:40:46 -0800

    Completed today's exercise

---
+++ Migrations/20230216224531_Writer Entity.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace IndyBooks.Migrations
+{
+    public partial class WriterEntity : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Author",
+                table: "Books");
+
+            migrationBuilder.AddColumn<long>(
+                name: "AuthorId",
+                table: "Books",
+                type: "bigint",
+                nullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "Writers",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Writers", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Books_AuthorId",
+                table: "Books",
+                column: "AuthorId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Books_Writers_AuthorId",
+                table: "Books",
+                column: "AuthorId",
+                principalTable: "Writers",
+                principalColumn: "Id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Books_Writers_AuthorId",
+                table: "Books");
+
+            migrationBuilder.DropTable(
+                name: "Writers");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Books_AuthorId",
+                table: "Books");
+
+            migrationBuilder.DropColumn(
+                name: "AuthorId",
+                table: "Books");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Author",
+                table: "Books",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+    }
+}
---
+++ Models/Writer.cs
@@ -0,0 +1,10 @@
+﻿namespace IndyBooks.Models
+{
+    public class Writer
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public ICollection<Book> Books { get; set;}
+        public string Bio { get; set; }
+    }
+}
